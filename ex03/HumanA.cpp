41592cd2fc661229b7c42f0c3ce66c49ea93b81d
Ayoub <aybelhaj@car7s5.42barcelona.com>
2025-11-28 20:02:31 +0100

    ex03 : p

---
+++ ex03/HumanA.cpp
@@ -0,0 +1,8 @@
+#include "HumanA.hpp"
+
+HumanA::HumanA(std::string _name, Weapon &_weapon):name(_name),weapon(_weapon)
+{}
+void HumanA::attack()
+{
+	std::cout<<name<<" attacks with their "<<weapon.getType()<<std::endl;
+}
---
+++ ex03/Weapon.hpp
@@ -0,0 +1,17 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <iostream>
+#include <string>
+
+class Weapon
+{
+	private:
+		std:: string type;
+	public:
+		Weapon(std::string _type);
+		const std::string& getType();
+		void setType(std::string _type);
+};
+
+#endif
